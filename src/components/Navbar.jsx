98718c41e466704d9c81739df7ae41f601b8322f
Austin Cumberlander <austincumberlander@gmail.com>
2023-12-09 17:56:30 -0600

    Adds code that checks the device to determine what CSS to apply to the tabs on the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,25 @@ import EventsScreen from '../screens/EventsScreen';
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+import { Platform } from 'react-native';
 
 const Tab = createBottomTabNavigator();
+
+const isAndroid = Platform.OS === 'android';
+const isIphone = Platform.OS === 'ios';
+let tabBarCSS;
+if (isAndroid) {
+  tabBarCSS = {
+    backgroundColor: '#FFD600',
+    height: 80,
+  }
+} else if (isIphone) {
+  tabBarCSS = {
+    backgroundColor: '#FFD600',
+    height: 130,
+    marginBottom: -30,
+  }
+}
 
 export const Navbar = () => {
   return (
@@ -24,16 +41,7 @@ export const Navbar = () => {
           );
         }
       },
-      tabBarStyle: {
-        backgroundColor: '#FFD600',
-        
-        // style 1 - without yellow space on bottom
-        // height: 130,
-        // marginBottom: -30
-
-        // style 2 - with yellow space on bottom
-        height: 100,
-      },
+      tabBarStyle: tabBarCSS,
       tabBarActiveBackgroundColor: '#000',
       tabBarInactiveBackgroundColor: '#FFD600',
       tabBarShowLabel: false,
