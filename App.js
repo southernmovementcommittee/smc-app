dfa0974c12acaf61bbd0c97179d2290fd15a965b
Tatiana Johnson <tatianarjo@gmail.com>
2023-12-08 20:10:46 -0600

    added sublline for dev branch

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Southern Movement Committee App</Text>
+      <Text>Event Management</Text>
       <StatusBar style="auto" />
     </View>
   );
