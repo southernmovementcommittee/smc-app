9e6c9bb12ab135fdb888f4ca8952c73e0dddb10e
Austin Cumberlander <austincumberlander@gmail.com>
2023-12-09 16:46:04 -0600

    Made CSS updates to navbar tabs.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,33 @@ const Tab = createBottomTabNavigator();
 export const Navbar = () => {
   return (
     <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
+      tabBarIcon: ({ color, size, focused }) => {
         let iconName;
+        size = 40
+        color = focused ? '#FFD600' : '#000';
 
-        if (route.name === "Home") {
-          iconName = "home";
-          return <FontAwesomeIcon name={iconName} size={size} color={'black'} />;
-        } else if (route.name === "Events") {
-          iconName = "ticket";
+        if (route.name === 'Home') {
+          iconName = 'home';
+          return <FontAwesomeIcon name={iconName} size={size} color={color} />;
+        } else if (route.name === 'Events') {
+          iconName = 'ticket';
           return (
-            <FontAwesomeIcon name={iconName} size={25} color={'black'} />
+            <FontAwesomeIcon name={iconName} size={size} color={color} />
           );
         }
       },
       tabBarStyle: {
         backgroundColor: '#FFD600',
+        
+        // style 1 - without yellow space on bottom
+        // height: 130,
+        // marginBottom: -30
+
+        // style 2 - with yellow space on bottom
+        height: 100,
       },
+      tabBarActiveBackgroundColor: '#000',
+      tabBarInactiveBackgroundColor: '#FFD600',
       tabBarShowLabel: false,
     })}
       
