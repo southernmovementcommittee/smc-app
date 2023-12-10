f3a9e168274c931669ed78908625a2f9c0821326
Austin Cumberlander <austincumberlander@gmail.com>
2023-12-10 16:42:13 -0600

    Adds UI for create account step 1.

--- src/screens/CreateAccountScreen/CreateAccountScreen2.jsx
+++ src/screens/CreateAccountScreen/CreateAccountScreen2.jsx
@@ -3,22 +3,22 @@ import { View, TextInput, Text, TouchableOpacity, Image  } from 'react-native';
 import { styles } from './CreateAccountScreenCSS';
 const smclogo = require('../../assets/images/smclogo.png');
 
-const CreateAccountScreen = ({ navigation }) => {
+const CreateAccountScreen2 = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={smclogo} />
       </View>
-      <Text style={styles.header} >CREATE NEW ACCOUNT</Text>
-      <TextInput placeholder="Email Address" style={styles.input} />
+      <Text style={styles.header} >CREATE NEW ACCOUNT 2</Text>
+      {/* <TextInput placeholder="Email Address" style={styles.input} />
       <TextInput placeholder="Password" secureTextEntry style={styles.input} />
-      <TextInput placeholder="Confirm Password" secureTextEntry style={styles.input} />
+      <TextInput placeholder="Confirm Password" secureTextEntry style={styles.input} /> */}
       <TouchableOpacity style={styles.primaryButton} onPress={() => navigation.navigate('Step2')}>
-        <Text style={styles.primaryButtonText} >Continue to Step 2</Text>
+        <Text style={styles.primaryButtonText} >Continue to Step 3</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 
-export default CreateAccountScreen;
+export default CreateAccountScreen2;
