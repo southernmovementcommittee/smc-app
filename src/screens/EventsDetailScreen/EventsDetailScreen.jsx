9fc18580046857e4121df580f8176f11ef5a66c3
Glenn Dixon <glenn.dixon@groups360.com>
2023-12-12 21:51:51 -0600

    Removed a few files, folders. Have the cards routing to the correct details page

---
+++ src/screens/EventsDetailScreen/EventsDetailScreen.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    SafeAreaView,
+    ScrollView,
+    ActivityIndicator,
+    RefreshControl,
+} from "react-native";
+
+const EventDetailsScreen = ({route, navigation }) => {
+    const { eventId, Title } = route.params;
+
+    // const { itemId } = route.params.eventId?.id
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <Text>Event Details for: {eventId}</Text>
+                <Text>{Title}</Text>
+
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default EventDetailsScreen
--- src/screens/EventsScreen/EventsScreen.jsx
+++ src/screens/EventsScreen/EventsScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './EventsScreenCSS';
+import EventList from '../../components/EventList/EventList';
 
-const EventsScreen = ({ navigation }) => {
+const EventsScreen = ({ navigation, route }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Events Screen</Text>
+      <EventList
+        navigation={navigation}
+        route={route}
+      />
     </View>
   );
 };
