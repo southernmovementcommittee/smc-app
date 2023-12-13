8f0d7a4d936aaeb78d9c82f08a27f9bdc59d5061
Austin Cumberlander <austincumberlander@gmail.com>
2023-12-13 17:11:29 -0600

    work in progress.

--- src/screens/CreateAccountScreen/CreateAccountScreen3.jsx
+++ src/screens/CreateAccountScreen/CreateAccountScreen3.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import { View, TextInput, Text, TouchableOpacity, Image  } from 'react-native';
-import { styles } from './CreateAccountScreenCSS';
+import React, {useState} from 'react';
+import { View, TextInput, Text, TouchableOpacity, Image, Button } from 'react-native';
+import DatePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { styles } from './CreateAccountScreen3CSS';
 const smclogo = require('../../assets/images/smclogo.png');
 
 const CreateAccountScreen3 = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={smclogo} />
-      </View>
-      <Text style={styles.header} >CREATE NEW ACCOUNT</Text>
-      <TextInput placeholder="Email Address" style={styles.input} />
-      <TextInput placeholder="Password" secureTextEntry style={styles.input} />
-      <TextInput placeholder="Confirm Password" secureTextEntry style={styles.input} />
-      <TouchableOpacity style={styles.primaryButton} onPress={() => navigation.navigate('Step2')}>
-        <Text style={styles.primaryButtonText} >Continue to Step 2</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  // const [date, setDate] = useState(new Date());
+
+  // return (
+  //   <View style={styles.container}>
+  //     <View style={styles.imageContainer}>
+  //       <Image style={styles.image} source={smclogo} />
+  //     </View>
+  //     <Text style={styles.header} >Upload your image here</Text>
+  //     <TextInput placeholder="OCCUPATION" style={styles.input} />
+  //     <DatePicker
+  //       date={date}
+  //       mode="date"
+  //       style={styles.input}
+  //       placeholder="DATE OF BIRTH"v
+  //       format="MM-DD=YYYY"
+  //       minDate="2016-05-01"
+  //       maxDate="2022-06-01"
+  //       confirmBtnText="Confirm"
+  //       cancelBtnText="Cancel"
+  //       onDateChange={(date) => setDate(date)}
+  //   />
+  //     {/* <TextInput placeholder="DATE OF BIRTH" dataDetectorTypes={'calendarEvent'} style={styles.input} /> */}
+  //     <TextInput
+  //       placeholder="BIO"
+  //       multiline={true}
+  //       numberOfLines={4}
+  //       style={styles.textArea} />
+  //     <TouchableOpacity style={styles.primaryButton} onPress={() => navigation.navigate('Step2')}>
+  //       <Text style={styles.primaryButtonText} >CREATE ACCOUNT</Text>
+  //     </TouchableOpacity>
+  //   </View>
+  // );
 };
 
 
