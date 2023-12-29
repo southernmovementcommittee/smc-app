092ec3832d86bfa845fe2f7580553a5adfddf9d3
Glenn Dixon <glenn.dixon@groups360.com>
2023-12-28 21:02:10 -0600

    pushing up file structure

--- server/src/index.js
+++ server/src/index.js
@@ -6,9 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/message', (req, res) => {
-    res.json({ message: 'Hello World!\n' })
-});
+// app.get('/message', (req, res) => {
+//     res.json({ message: 'Hello World!\n' })
+// });
+
+app.use(express.json());
+app.use('/api', emailRoutes);
 
 app.listen(8000, () => {
     console.log(`Server running at 8000`)
