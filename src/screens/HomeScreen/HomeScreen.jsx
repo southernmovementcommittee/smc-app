44cb60bda862e37967c9a917cab670a2181411bd
Tatiana Johnson <tatianarjo@gmail.com>
2024-01-12 19:59:16 -0600

    added mass assembly photo to home screen and events photo

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, TouchableOpacity  } from 'react-native';
+import { View, Text, TouchableOpacity, Image  } from 'react-native';
 import { styles } from './HomeScreenCSS';
+
+const massassembly = require('../../assets/images/massassembly.jpg');
 
 const HomeScreen = ({ user, setIsAuth }) => {
   return (
     <View style={styles.container}>
+      <Image  source={massassembly} style={{width: 400, height: 400}}  />
       <Text style={styles.text}>Home Screen</Text>
       <View style={styles.textContainer}>
         <Text style={styles.text}>Newly Created User</Text>
